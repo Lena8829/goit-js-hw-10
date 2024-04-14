4abc5e5173e062bc2fa8e57d87524d622569cd7a
Lena8829 <illenarionova@gmail.com>
2024-04-15 01:22:08 +0300

    task-1.1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,9 @@
-// Напиши скрипт таймера, який здійснює зворотний відлік до певної дати.
-// оголоси поза межами методу let змінну, наприклад, userSelectedDate, і після валідації її в методі onClose() на минуле/майбутнє запиши обрану дату в цю let змінну.
+//! Напиши скрипт таймера, який здійснює зворотний відлік до певної дати.
+//* оголоси поза межами методу let змінну, наприклад, userSelectedDate, і після валідації її в методі onClose() на минуле/майбутнє запиши обрану дату в цю let змінну.
 
-// Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future" і зроби кнопку «Start» не активною.
-// Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
+//* Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future" і зроби кнопку «Start» не активною.
+//* Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
+
 // Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому. Зверни увагу, що при обранні валідної дати, не запуску таймера і обранні потім невалідної дати, кнопка після розблокування має знову стати неактивною.
 // Натисканням на кнопку «Start» починається зворотний відлік часу до обраної дати з моменту натискання.
 
@@ -14,32 +15,71 @@
 
 // Після запуску таймера натисканням кнопки Старт кнопка Старт і інпут стають неактивним, щоб користувач не міг обрати нову дату, поки йде відлік часу. Якщо таймер запущений, для того щоб вибрати нову дату і перезапустити його — необхідно перезавантажити сторінку.
 
-// Форматування часу:
-// Функція convertMs() повертає об'єкт з розрахованим часом, що залишився до кінцевої дати. Зверни увагу, що вона не форматує результат. Тобто якщо залишилося 4 хвилини або будь-якої іншої складової часу, то функція поверне 4, а не 04. В інтерфейсі таймера необхідно додавати 0, якщо в числі менше двох символів. Напиши функцію, наприклад addLeadingZero(value), яка використовує метод рядка padStart() і перед відмальовуванням інтерфейсу форматує значення.
-
-// let userSelectedDate;
-
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     userSelectedDate = selectedDates[0];
-//     if (userSelectedDate < Date.now()) {
-//       iziToast.error({
-//         message: 'Please choose a date in the future',
-//         position: 'topRight',
-//       });
-//       startButon.disabled = true;
-//     } else {
-//       startButon.disabled = false;
-//     }
-//   },
-// };
+//* Форматування часу:
+//* Функція convertMs() повертає об'єкт з розрахованим часом, що залишився до кінцевої дати. Зверни увагу, що вона не форматує результат. Тобто якщо залишилося 4 хвилини або будь-якої іншої складової часу, то функція поверне 4, а не 04. В інтерфейсі таймера необхідно додавати 0, якщо в числі менше двох символів. Напиши функцію, наприклад addLeadingZero(value), яка використовує метод рядка padStart() і перед відмальовуванням інтерфейсу форматує значення.
 
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+//----------------------------------------------------------------------------------
+const datetimePicker = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('[data-start]');
+
+startButton.addEventListener('click', startTimer);
+
+let userSelectedDate;
+
+//налаштування flatppickr------------------------------------------------------------
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0]; // Отримуємо обрану дату
+    const currentDate = new Date();
+
+    if (userSelectedDate < currentDate) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+
+      startButton.disabled = true; // Вимикаємо кнопку
+    } else {
+      startButton.disabled = false; // Активуємо
+    }
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options);
+//------------------------------------------------------------------------------------
+
+function startTimer() {}
+
+//конвертація мілісекунд--------------------------------------------------------------
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+//----------------------------------------------------------------------------------
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0'); //додаєм "0" вперед одинарної цифри
+}
