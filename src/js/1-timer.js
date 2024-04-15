6a872e4ec2d5804679b574ce958676b86c9eee39
Lena8829 <illenarionova@gmail.com>
2024-04-15 12:23:02 +0300

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,16 +4,16 @@
 //* Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future" і зроби кнопку «Start» не активною.
 //* Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
 
-// Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому. Зверни увагу, що при обранні валідної дати, не запуску таймера і обранні потім невалідної дати, кнопка після розблокування має знову стати неактивною.
+//* Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому. Зверни увагу, що при обранні валідної дати, не запуску таймера і обранні потім невалідної дати, кнопка після розблокування має знову стати неактивною.
 // Натисканням на кнопку «Start» починається зворотний відлік часу до обраної дати з моменту натискання.
 
 // Відлік часу:
 // Натисканням на кнопку «Start» скрипт повинен обчислювати раз на секунду, скільки часу залишилось до вказаної дати, і оновлювати інтерфейс таймера, показуючи чотири цифри: дні, години, хвилини і секунди у форматі xx:xx:xx:xx.
 
 // Кількість днів може складатися з більше, ніж двох цифр.
-// Таймер повинен зупинятися, коли дійшов до кінцевої дати, тобто залишок часу дорівнює нулю 00:00:00:00.
+//* Таймер повинен зупинятися, коли дійшов до кінцевої дати, тобто залишок часу дорівнює нулю 00:00:00:00.
 
-// Після запуску таймера натисканням кнопки Старт кнопка Старт і інпут стають неактивним, щоб користувач не міг обрати нову дату, поки йде відлік часу. Якщо таймер запущений, для того щоб вибрати нову дату і перезапустити його — необхідно перезавантажити сторінку.
+// *Після запуску таймера натисканням кнопки Старт кнопка Старт і інпут стають неактивним, щоб користувач не міг обрати нову дату, поки йде відлік часу. Якщо таймер запущений, для того щоб вибрати нову дату і перезапустити його — необхідно перезавантажити сторінку.
 
 //* Форматування часу:
 //* Функція convertMs() повертає об'єкт з розрахованим часом, що залишився до кінцевої дати. Зверни увагу, що вона не форматує результат. Тобто якщо залишилося 4 хвилини або будь-якої іншої складової часу, то функція поверне 4, а не 04. В інтерфейсі таймера необхідно додавати 0, якщо в числі менше двох символів. Напиши функцію, наприклад addLeadingZero(value), яка використовує метод рядка padStart() і перед відмальовуванням інтерфейсу форматує значення.
@@ -26,10 +26,15 @@ import 'izitoast/dist/css/iziToast.min.css';
 //----------------------------------------------------------------------------------
 const datetimePicker = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('[data-start]');
+const timerDay = document.querySelector(['data-days']);
+const timerHour = document.querySelector(['data-hours']);
+const timerMinute = document.querySelector(['data-minutes']);
+const timerSecond = document.querySelector(['data-seconds']);
 
 startButton.addEventListener('click', startTimer);
 
 let userSelectedDate;
+let timerInterval;
 
 //налаштування flatppickr------------------------------------------------------------
 const options = {
@@ -58,8 +63,6 @@ const options = {
 flatpickr('#datetime-picker', options);
 //------------------------------------------------------------------------------------
 
-function startTimer() {}
-
 //конвертація мілісекунд--------------------------------------------------------------
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -82,4 +85,55 @@ console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0'); //додаєм "0" вперед одинарної цифри
+
+  // return value < 10 ? `0${value}` : value;
+}
+//-----------------------------------------------------------------------------------
+
+//відображення таймера(скільки лишилось часу)---------------------------------
+function timerUpdate(ms) {
+  const { days, hours, minutes, seconds } = convertMs(ms);
+
+  timerDay.textContent = addLeadingZero(days);
+  timerHour.textContent = addLeadingZero(hours);
+  timerMinute.textContent = addLeadingZero(minutes);
+  timerSecond.textContent = addLeadingZero(seconds);
+}
+//-------------------------------------------------------------------------------------
+
+function startTimer() {
+  // Робимо кнопку "Start" та поле вибору дати неактивними
+  startButton.disabled = true;
+  datetimePicker.disabled = true;
+
+  const currentDate = new Date();
+  const timeDiff = userSelectedDate - currentDate; // Визначаємо різницю у часі
+
+  //зупиняємо таймер, коли вже 0
+  if (timeDiff <= 0) {
+    clearInterval(timerInterval);
+
+    timerUpdate(0); // Відображаємо 00:00:00:00
+    startButton.disabled = false; // знову активно
+    datetimePicker.disabled = false;
+    return;
+  }
+
+  // Оновлюємо таймер кожну секунду
+  timerInterval = setInterval(() => {
+    const currentDate = new Date();
+    const timeDiff = userSelectedDate - currentDate;
+
+    if (timeDiff <= 0) {
+      clearInterval(timerInterval);
+      timerUpdate(0); // Відображаємо 00:00:00:00
+      startButton.disabled = false; // знову активно
+      datetimePicker.disabled = false;
+      return;
+    }
+
+    timerUpdate(timeDiff);
+  }, 1000);
+
+  timerUpdate(timeDiff);
 }
